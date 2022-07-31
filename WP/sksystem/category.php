cde76fcfd4b47d86466cf286d8a60b83274c76ff
chantare <65115371+chantare@users.noreply.github.com>
2022-07-31 14:41:58 +0900

    マガジンのカテゴリを表示

--- WP/sksystem/category.php
+++ WP/sksystem/category.php
@@ -1,10 +1,10 @@
 <?php
 $post = $wp_query->post;
-if (in_category("info")) {
-    include(TEMPLATEPATH . "/category-info.php");
-} elseif (in_category("blog")) {
+if (in_category("construction")) {
+    include(TEMPLATEPATH . "/category-construction.php");
+} elseif (in_category("magazine")) {
     include(TEMPLATEPATH . "/category-magazine.php");
 } else {
-    include(TEMPLATEPATH . '/category-info.php');
+    include(TEMPLATEPATH . '/category-magazine.php');
 }
 ?>
