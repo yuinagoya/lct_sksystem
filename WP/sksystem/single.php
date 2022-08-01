da4264103efbbade3f53b7d3b9802828510e25ea
chantare <65115371+chantare@users.noreply.github.com>
2022-08-01 10:42:34 +0900

    最新のHTMLの内容で更新

--- WP/sksystem/single.php
+++ WP/sksystem/single.php
@@ -1,30 +1,41 @@
 <?php get_header(); ?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <main id="main">
-      <article>
-        <div class="wrap">
-          <section class="single">
-            <?php the_title('<h1>', '</h1>'); ?>
-            <time class="post-date"><?php the_time('Y年m月d日'); ?></time>
-            <?php if (has_post_thumbnail()) : ?>
-              <?php the_post_thumbnail(); ?>
-            <?php endif; ?>
 
-            <div class="post-content">
-              <?php the_content(); ?>
-            </div>
-            <!-- /.post-content -->
-          </section>
-
-          <nav class="pager">
-            <?php previous_post_link("%link", "&lt; %title"); ?>
-            <?php next_post_link("%link", "%title &gt;"); ?>
-          </nav>
+  <main id="main">
+    <article>
+      <section class="mv">
+        <div class="mv__img">
+          <picture>
+            <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/images/news/sp/mv.png">
+            <img src="<?php bloginfo('template_directory'); ?>/images/news/mv.png" alt="MV" loading="lazy">
+          </picture>
         </div>
-        <!-- /.wrap -->
-      </article>
-    </main>
+        <div class="mv__txt">
+          <h1>
+            <span class="ja">最新情報</span>
+            <span class="en">
+              <picture>
+                <img src="<?php bloginfo('template_directory'); ?>/images/news/mv_en.png" alt="New" loading="lazy">
+              </picture>
+            </span>
+          </h1>
+        </div>
 
-<?php endwhile;
-endif; ?>
+        <ul class="breadcrumb">
+          <li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">ホーム</a></li>
+          <li class="home"><a href="<?php echo esc_url( home_url( '/news/' ) ); ?>"><span class="arrow">&gt;</span>最新情報</a></li>
+          <li class="crrent"><span class="arrow">&gt;</span><?php the_title(); ?></li>
+        </ul>
+      </section>
+
+      <section class="single">
+        <div class="wrap">
+          <h1><?php the_title(); ?></h1>
+          <time class="post-date"><?php the_time('Y年m月d日'); ?></time>
+          <div class="post-content">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </section>
+    </article>
+  </main>
 <?php get_footer(); ?>
