267f98b3b6b41d348f05305b2fb61d1069415f6f
Yoshiki Tanaka <info@yoshiki-tanaka.work>
2022-08-01 18:45:16 +0900

    TOP施工事例修正

--- WP/sksystem/thum_construction.php
+++ WP/sksystem/thum_construction.php
@@ -1,30 +1,34 @@
 <?php
-  $query = new WP_Query(
-    array(
-      'orderby' => 'date',
-      'order' => 'DESC',
-      'category_name' => $param_category,
-      'posts_per_page' => $param_count,
-    )
-  );
+$query = new WP_Query(
+  array(
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'category_name' => $param_category,
+    'posts_per_page' => $param_count,
+  )
+);
 ?>
-<?php if ( $query->have_posts() ): ?>
+<?php if ($query->have_posts()) : ?>
   <ul class="state-list">
 
-  <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+    <?php while ($query->have_posts()) : $query->the_post(); ?>
 
-    <li class="scrollin-3col">
-      <a href="<?php the_permalink(); ?>">
-        <?php if (has_post_thumbnail()) : ?>
-          <?php the_post_thumbnail('thumbnail'); ?>
-        <?php else : ?>
-          <img src="<?php bloginfo('template_directory'); ?>/images/case/dummy.jpg" alt="" loading="lazy">
-        <?php endif ; ?>
-        <time><?php the_time( 'Y.m.d' ); ?></time>
-        <p><?php the_title(); ?></p>
-      </a>
-    </li>
-  <?php endwhile; ?>
+      <li class="scrollin-3col">
+        <a href="<?php the_permalink(); ?>">
+          <?php if (has_post_thumbnail()) : ?>
+            <figure>
+              <?php the_post_thumbnail('thumbnail'); ?>
+            </figure>
+          <?php else : ?>
+            <figure>
+              <img src="<?php bloginfo('template_directory'); ?>/images/case/dummy.jpg" alt="" loading="lazy">
+            </figure>
+          <?php endif; ?>
+          <time><?php the_time('Y.m.d'); ?></time>
+          <p><?php the_title(); ?></p>
+        </a>
+      </li>
+    <?php endwhile; ?>
 
   </ul>
 
