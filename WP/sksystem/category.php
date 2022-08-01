7cf4abb50a7412e64176ffb257b786a20b066590
chantare <65115371+chantare@users.noreply.github.com>
2022-08-01 10:25:41 +0900

    カテゴリを絞り込むは、イベント情報、お知らせ、ブログのみ表示する

--- WP/sksystem/category.php
+++ WP/sksystem/category.php
@@ -4,6 +4,12 @@ if (in_category("construction")) {
     include(TEMPLATEPATH . "/category-construction.php");
 } elseif (in_category("magazine")) {
     include(TEMPLATEPATH . "/category-magazine.php");
+} elseif (is_category("event")) {
+    include(TEMPLATEPATH . "/category-info.php");
+} elseif (is_category("blog")) {
+    include(TEMPLATEPATH . "/category-info.php");
+} elseif (is_category("cate_news")) {
+    include(TEMPLATEPATH . "/category-info.php");
 } else {
     include(TEMPLATEPATH . '/category-magazine.php');
 }
