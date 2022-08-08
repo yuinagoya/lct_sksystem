809ec5599a0e83bbc14e49b048c7b66085dfdd81
chantare <65115371+chantare@users.noreply.github.com>
2022-08-08 15:50:29 +0900

    サンクスページのMV対応

--- WP/sksystem/page.php
+++ WP/sksystem/page.php
@@ -1,11 +1,19 @@
 <?php get_header(); ?>
+
+<?php
+  $mv_name = $post->post_name;
+  if($mv_name == "thanks"){
+    $mv_name = "contact";
+  }
+?>
+
   <main id="main" <?php if(is_page('line') ){echo "class='page-line'";} elseif(is_page('qa')){ echo "class='page-faq'"; } elseif(is_page('staff')){ echo "class='page-staff'"; }; ?>>
     <article>
       <section class="mv">
         <div class="mv__img">
           <picture>
-            <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/images/<?php echo $post->post_name; ?>/sp/mv.png">
-            <img src="<?php bloginfo('template_directory'); ?>/images/<?php echo $post->post_name; ?>/mv.png" alt="MV" loading="lazy">
+            <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/images/<?php echo $mv_name; ?>/sp/mv.png">
+            <img src="<?php bloginfo('template_directory'); ?>/images/<?php echo $mv_name; ?>/mv.png" alt="MV" loading="lazy">
           </picture>
         </div>
         <!-- /.mv__img -->
@@ -15,7 +23,7 @@
             <span class="ja"><?php the_title(); ?></span>
             <span class="en">
               <picture>
-                <img src="<?php bloginfo('template_directory'); ?>/images/<?php echo $post->post_name; ?>/mv_en.png" alt="<?php the_title(); ?>" loading="lazy">
+                <img src="<?php bloginfo('template_directory'); ?>/images/<?php echo $mv_name; ?>/mv_en.png" alt="<?php the_title(); ?>" loading="lazy">
               </picture>
             </span>
           </h1>
