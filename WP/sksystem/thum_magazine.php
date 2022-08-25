00f15aaa4cd0b8b60cfa61827ffd60cf9218e196
chantare <65115371+chantare@users.noreply.github.com>
2022-08-25 18:21:08 +0900

    アイキャッチがない場合は1番目の画像を表示する

--- WP/sksystem/thum_magazine.php
+++ WP/sksystem/thum_magazine.php
@@ -19,8 +19,8 @@
           <?php if (has_post_thumbnail()) : ?>
             <?php the_post_thumbnail('thumbnail'); ?>
           <?php else : ?>
-            <!--<img src="<?php bloginfo('template_directory'); ?>/images/top/magazine_01.jpg" alt="" loading="lazy">-->
-            <img src="<?php bloginfo('template_directory'); ?>/images/case/dummy.jpg" alt="" loading="lazy">
+            <!--<img src="<?php bloginfo('template_directory'); ?>/images/case/dummy.jpg" alt="" loading="lazy">-->
+            <img src="<?php echo catch_that_image(); ?>" alt="" loading="lazy">
           <?php endif ; ?>
         </a>
       </div>
