0aecc4422b1ef75ea0d0be44f6d45659212399a2
chantare <65115371+chantare@users.noreply.github.com>
2022-08-25 18:20:40 +0900

    アイキャッチがない場合は1番目の画像を表示する

--- WP/sksystem/category-magazine.php
+++ WP/sksystem/category-magazine.php
@@ -60,8 +60,8 @@
                     <?php if (has_post_thumbnail()) : ?>
                       <?php the_post_thumbnail('thumbnail'); ?>
                     <?php else : ?>
-                      <!--<img src="<?php bloginfo('template_directory'); ?>/images/magazine/magazine_catimg01.jpg" alt="" loading="lazy">-->
-                      <img src="<?php bloginfo('template_directory'); ?>/images/case/dummy.jpg" alt="" loading="lazy">
+                      <!--<img src="<?php bloginfo('template_directory'); ?>/images/case/dummy.jpg" alt="" loading="lazy">-->
+                      <img src="<?php echo catch_that_image(); ?>" alt="" loading="lazy">
                     <?php endif ; ?>
                     <div class="magazine-list__date"><?php the_time( 'Y.m.d' ); ?></div>
                   </a>
